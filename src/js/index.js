f7dec3cd983d306b6e7c46061620c66e01254317
Genezis <ucmessina@alumnostrazos.net>
2025-01-17 11:59:40 +0100

    +Update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+const hamburgerButton = document.getElementById('header__hamburger');
+const menuElement = document.getElementById('menu');
+
+const toggleMenu = () => {
+  menuElement.classList.toggle('menu--active');
+
+  const isMenuActive = menuElement.classList.contains('menu--active');
+  hamburgerButton.src = isMenuActive
+    ? './assets/images/mobile/icon-cross.svg'
+    : './assets/images/mobile/icon-hamburger.svg';
+};
+
+hamburgerButton.addEventListener('click', toggleMenu);
